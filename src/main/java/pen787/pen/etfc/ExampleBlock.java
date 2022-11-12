1cb07ba02d83e81a61197086f76dcc5dc7d70f53
pen787 <peninm7694638@gmail.com>
2022-11-12 18:40:52 +0700

    add new test block

---
+++ src/main/java/pen787/pen/etfc/ExampleBlock.java
@@ -0,0 +1,29 @@
+package pen787.pen.etfc;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class ExampleBlock extends Block {
+
+    public ExampleBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+        if (!world.isClient) {
+            player.sendMessage(Text.of("Hello, world!"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
